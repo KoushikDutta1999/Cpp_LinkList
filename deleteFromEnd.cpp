0fd75fbc410b0fdfda4a2308dac5cea9dd65cfe9
KoushikDutta99 <koushikdutta.bca2019@nsec.ac.in>
2021-10-23 21:22:22 +0530

    first commit

---
+++ deleteFromEnd.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+class node {
+public:
+  int data;
+  node *next;
+
+  node(int data) {
+    this->data = data;
+    this->next = NULL;
+  }
+};
+
+node *add(node *root) {
+  int data;
+  cin >> data;
+  if (root == NULL) {
+    root = new node(data);
+    return root;
+  }
+
+  node *temp = root;
+  while (temp->next != NULL) {
+    temp = temp->next;
+  }
+  temp->next = new node(data);
+  return root;
+}
+
+void print(node *root) {
+  while (root != NULL) {
+    cout << root->data << " ";
+    root = root->next;
+  }
+  return;
+}
+
+// delete at the end of a linked list
+
+node *deleteFromEnd(node *root) {
+  if (root == NULL)
+    return root;
+
+  node *prev = NULL;
+  node *curr = root;
+
+  while (curr->next != NULL) {
+    prev = curr;
+    curr = curr->next;
+  }
+
+  prev->next = NULL;
+  // delete curr;
+  return root;
+}
+
+int main() {
+  // your code goes here
+  node *root = NULL;
+  cout << "Enter number of nodes you want to enter to the linkedlist: ";
+  int n;
+  cin >> n;
+
+  for (int i = 1; i <= n; i++) {
+    root = add(root);
+  }
+  print(root);
+  cout << endl;
+
+  root = deleteFromEnd(root);
+  print(root);
+
+  return 0;
+}
